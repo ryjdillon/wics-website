e164af6eb4046cc825245a659d185b1547ba912b
LAPTOP-7I1JN100\ryrya <ryryandillon@gmail.com>
2022-12-22 16:58:51 -0500

    header and desc.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="title">
+        <h1>WPI Women in Computer Science</h1>
+      </div>
+      <div>
+        <p>
+          Women in Computer Science (WiCS) at Worcester Polytechnic Institute is a student run organization on campus with the goal of supporting as many students as possible. 
+          We aim to aid in classes and relieving stress, as well as creating lasting friendships on campus. WiCS is here to support students' studies, 
+          opportunities, friendships, and their overall college experiences. We are here to help educate and promote women in computer science and related fields. 
+          To do this, we host events that connect students to professional opportunities, bring companies to campus, prepare them for classes, 
+          give them resources, as well as host coding competitions, professional workshops, movie nights, and provide upperclassmen help.
+        </p>
+      </div>
     </div>
   );
 }
